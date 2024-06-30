6eba832e95f8d89e03bb8d959c6179a436a3e7e4
Graham McKinlay <3449059+gmckinlay@users.noreply.github.com>
2024-07-01 00:07:35 +0100

    Various updates to improve rewards

--- reward_function.py
+++ reward_function.py
@@ -1,7 +1,7 @@
 import math
 
 class Reward:
-    def __init__(self, verbose=False, look_ahead_alignment=6, look_ahead_speed=10):
+    def __init__(self, verbose=False, look_ahead_alignment=3, look_ahead_speed=10):
         self.verbose = verbose
         self.prev_speed = 0
         self.prev_steering_angle = 0
@@ -42,38 +42,49 @@ class Reward:
         
         return angle_deg        
 
-    def get_steering_alignment_reward(self, waypoints, next_points, heading):
+    def get_steering_reward(self, waypoints, next_points, heading, steering_angle):
         start_point = waypoints[next_points[0]]
         forward_point = waypoints[next_points[-1]]
-        
         dy = forward_point[1] - start_point[1]
         dx = forward_point[0] - start_point[0]
         optimal_heading = math.degrees(math.atan2(dy, dx))
-        
         heading_diff = abs(optimal_heading - heading)
         if heading_diff > 180:
             heading_diff = 360 - heading_diff
 
-        result = self.logarithmic_decreasing_function(heading_diff, 180)
-        return result
-        
-    def get_steering_smoothness_reward(self, steering_angle):
-        prev_steering_angle = self.prev_steering_angle
+        # Combine alignment and smoothness into a single reward
+        steering_diff = abs(steering_angle - self.prev_steering_angle)
         self.prev_steering_angle = steering_angle
-        steering_diff = abs(steering_angle - prev_steering_angle)        
-        result = self.logarithmic_decreasing_function(steering_diff, 60)
+
+        alignment_reward = self.logarithmic_decreasing_function(heading_diff, 180)
+        smoothness_reward = self.logarithmic_decreasing_function(steering_diff, 60)
+        
+        result = 0.5 * alignment_reward + 0.5 * smoothness_reward
+        self.print("get_steering_reward", result)
         return result
 
-    def get_speed_reward(self, speed, waypoints, next_points, initial_point):
-        next_point = waypoints[next_points[len(next_points) // 2]]
+    def get_speed_reward(self, speed, waypoints, next_points):
+        start_point = waypoints[next_points[0]]
+        mid_point = waypoints[next_points[len(next_points) // 2]]
         forward_point = waypoints[next_points[-1]]
-        curvature = self.angle_between_points(initial_point, next_point, forward_point)
+        curvature = self.angle_between_points(start_point, mid_point, forward_point)
 
-        min_speed, max_speed = 1, 3.5
-        optimal_speed = max_speed - (curvature / 180) * (max_speed - min_speed)
-        
+        # Define optimal speeds for different curvature ranges
+        if curvature < 10:
+            optimal_speed = 3.0  # High speed for straight paths
+        elif curvature < 30:
+            optimal_speed = 2.5
+        else:
+            optimal_speed = 1.0  # Low speed for sharper turns
+
+        # Calculate the speed difference
         speed_diff = abs(speed - optimal_speed)
-        result = self.logarithmic_decreasing_function(speed_diff, max_speed-min_speed)
+
+        # Reward function based on the speed difference
+        max_speed_diff = 2.0  # Maximum possible difference between speed options
+        result = self.logistic_decreasing_function(speed_diff, max_speed_diff)
+        
+        self.print("Speed Reward: Speed =", speed, "Optimal Speed =", optimal_speed, "Reward =", result)
         return result
     
     def get_center_line_reward(self, track_width, distance_from_center):
@@ -106,22 +117,25 @@ class Reward:
         
             heading = params['heading']
             steering_angle = params['steering_angle']
-            speed = params['speed']
-            location = (params['x'], params['y'])
+            speed = params['speed']            
 
-            next_points_alignment = self.find_next_waypoints(waypoints, closest_waypoints, self.look_ahead_alignment)
+            next_points_steering = self.find_next_waypoints(waypoints, closest_waypoints, self.look_ahead_alignment)
             next_points_speed = self.find_next_waypoints(waypoints, closest_waypoints, self.look_ahead_speed)
             
             center_line_reward = self.get_center_line_reward(track_width, distance_from_center)
-            steering_alignment_reward = self.get_steering_alignment_reward(waypoints, next_points_alignment, heading)
-            steering_smoothness_reward = self.get_steering_smoothness_reward(steering_angle)
-            speed_reward = self.get_speed_reward(speed, waypoints, next_points_speed, waypoints[closest_waypoints[0]])
+            steering_reward = self.get_steering_reward(waypoints, next_points_steering, heading, steering_angle)
+            speed_reward = self.get_speed_reward(speed, waypoints, next_points_speed)
 
-            initial_reward = (progress / 100)  # Adjusted scaling factor
-            reward += initial_reward * 1.5 * center_line_reward
-            reward += initial_reward * 1.0 * steering_alignment_reward
-            reward += initial_reward * 0.5 * steering_smoothness_reward
-            reward += initial_reward * 1.0 * speed_reward
+            # Combined progress and efficiency reward
+            progress_reward = progress / 100
+            efficiency_reward = progress / steps
+            combined_progress_efficiency_reward = 0.5 * progress_reward + 0.5 * efficiency_reward
+
+            # Combine all rewards
+            reward += center_line_reward * 0.3        
+            reward += speed_reward * 0.35
+            reward += combined_progress_efficiency_reward * 0.3            
+            reward += steering_reward * 0.05
 
             self.print("reward", reward)
         return float(reward)
